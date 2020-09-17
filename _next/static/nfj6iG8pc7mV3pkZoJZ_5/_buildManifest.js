7a505ae55c7f5e40ba54d92e439fd70e8f18a91b
pandemuliada <pandemuliada@gmail.com>
2020-09-17 13:38:35 +0000

    Deploying to gh-pages from  @ ce60bba21e4421e64c112cc662ac20b4eeea25cf 🚀

---
+++ _next/static/nfj6iG8pc7mV3pkZoJZ_5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.8093ec8c17384f92ab57.js","static\u002Fchunks\u002Fpages\u002Findex-d7c6974bd467004935f3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4c0e59626af2fe2052a1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/oPw2aZzRChASn--OInOez/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.8093ec8c17384f92ab57.js","static\u002Fchunks\u002Fpages\u002Findex-47d6fd977d60eec19a82.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4c0e59626af2fe2052a1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
